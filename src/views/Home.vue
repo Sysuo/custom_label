8b28f78cb05cf4720d337cfdfd23ab851dc0a7de
AKurosawa <a-kurosawa@mi-labo.coljp>
2018-11-24 23:13:07 +0900

    vuedraggableインストール

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="home">
+    <draggable :options="{ group: 'tasks' }" element="ul" v-for="list in tasks">
+      <li v-for="task in list">{{ task }}</li>
+    </draggable>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import draggable from 'vuedraggable';
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+export default {
+    name: 'home',
+    components: {
+        draggable,
+    },
+    data() {
+        return {
+            tasks: [
+                ['task1-1', 'task1-2', 'task1-3', 'task1-4'],
+                ['task2-1', 'task2-2', 'task2-3', 'task2-4'],
+                ['task3-1', 'task3-2', 'task3-3', 'task3-4'],
+            ]
+
+        }
+    }
+}
+
 </script>
